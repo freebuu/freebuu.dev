33bb48c4e6118377c677e6e3baf5c6b6c508b338
Maksim Mikhailov <2618094@gmail.com>
2023-07-01 02:02:59 +0300

    basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+import image from "@astrojs/image";
+
+export default defineConfig({
+  integrations: [mdx(), sitemap(), tailwind(), image(
+    {
+      serviceEntryPoint: '@astrojs/image/sharp',
+      cacheDir: "./.cache/image",
+      logLevel: 'debug',
+    }
+  )]
+});
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Portfolio';
+export const SITE_DESCRIPTION = 'Portfolio';
