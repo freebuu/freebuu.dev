b50d33bd0152e91f0ef94bbcb674f42f54cadba9
Maksim Mikhailov <2618094@gmail.com>
2023-07-01 15:57:02 +0300

    profile component and clean

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
-
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  experimental: {
+    assets: true
+   },
+  integrations: [mdx(), tailwind()]
 });
