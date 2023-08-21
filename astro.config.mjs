c7d2b8b7e1414b1546a4fd7fdfdc48ca948635b0
Maksim Mikhailov <2618094@gmail.com>
2023-08-21 19:56:23 +0200

    update address

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://freebuu.github.io',
+  site: 'https://freebuu.dev',
   experimental: {
     assets: true
    },
